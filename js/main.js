cdfc7bbde4ccf0e592a565d845784abfd9a8b742
Cristian Hourcade <hourcadecristian@gmail.com>
2021-09-24 16:31:15 -0300

    Metodos de array

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const producto = { id: 1, name: 'toto', price: 999, stock: 10 };
-const producto2 = { id: 2, name: 'niky', price: 1231, stock: 10 };
+const producto2 = { toto: 'holaaaa', ...producto };
 const producto3 = { id: 3, name: 'toto', price: 911111, stock: 10 };
 const producto4 = { id: 4, name: 'toto', price: 10, stock: 10 };
 const producto5 = { id: 5, name: 'toto', price: 23232, stock: 10 };
@@ -16,8 +16,40 @@ miTienda.agregarAlCarrito(producto2);
 miTienda.agregarAlCarrito(producto3);
 
 
-const totalPrecioCarrito  = miTienda.calcularTotal();
-console.log(totalPrecioCarrito);
+// console.log(miTienda.carrito.join(':)'))
+
+
+const totalPrecioCarrito = miTienda.calcularTotal();
+// console.log(totalPrecioCarrito);
 
 const cantidadDeElementos = miTienda.calcularCantidadElementos();
-console.log(cantidadDeElementos);
+// console.log(cantidadDeElementos);
+
+
+
+
+const array1 = [1, 2, 3];
+
+console.log(typeof saludar)
+
+const array2 = [3, 4, 5];
+const array3 = [6, 23, 125];
+const array4 = [53, 954, 58];
+
+// console.log(array1);
+// console.log(...array1);
+
+
+function saludar(){
+
+}
+
+const resultado = ['hola', ...array1, ...array2, ...array3, ...array4, 'hola'];
+console.log("|---------------------------------------------------------------|")
+resultado.forEach((element) => {
+    console.log(element + ',');
+})
+console.log("|---------------------------------------------------------------|")
+
+console.log(resultado.slice(0, 2));
+// Spread operator
