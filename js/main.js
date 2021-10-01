9c893855a652b3172b554399e675b77a0391a361
Cristian Hourcade <hourcadecristian@gmail.com>
2021-10-01 13:33:08 -0300

    Clase de eventos

--- js/main.js
+++ js/main.js
@@ -1,68 +1,32 @@
-/**
- * Productos done!
- * Carrito done!
- * 
- * Funcionalidades
- * 
- * agregarAlCarrito Done!
- * SacarDelCarrito
- * SumarTotal Done!
- * Contarelementos Done!
- * 
- * mostrarLascards Done!
- */
+//  getElementById toma por id un elemento
+//  getElementsByClassName toma los elementos de la misma clase array
+//  getElementsByTagName toma los elementos de la misma etiqueta array
 
-const CARRITO = [];
+const li = document.getElementsByTagName("li") // Array de tipo HTML Collection
 
-const PRODUCTOS = [
-    {id:1, name:"toto1", price:900},
-    {id:2, name:"toto2", price:978},
-    {id:3, name:"toto3", price:345},
-    {id:4, name:"toto4", price:89123},
-    {id:5, name:"toto5", price:111}
-]
-
-mostrarCards();
-
-function mostrarCards(){
-    let acumular = ``;
-    PRODUCTOS.forEach(producto => {
-        acumular += `<div>
-            <h1>${producto.name}</h1>
-            <h2>$${producto.price}</h2>
-            <button onclick='agregarAlCarrito(${producto.id})'>Agregar al carrito</button>
-            <button onclick='eliminarDelCarrito(${producto.id})'>Eliminar</button>
-        </div>`
-    });
-    document.getElementById("productos").innerHTML = acumular;
-}
-
-function eliminarDelCarrito(id){
-    const index = CARRITO.findIndex(producto => producto.id === id);
-    if(index !== -1){
-        CARRITO.splice(index,1);
+Array.from(li).forEach(elemento => {
+    elemento.onclick = (event) => {
+        location.href= "/"+event.target.getAttribute("data-route");
     }
-    
-     // Total del carrito
-     let total = 0;
-     CARRITO.forEach(producto => {
-         total += producto.price
-     });
-     document.getElementById("total").innerHTML = "$"+total;
- 
-     document.getElementById("cantidad").innerHTML = `Cantidad: ${CARRITO.length}`
-}
+})
 
-function agregarAlCarrito(id){
-    const productoAAgregar = PRODUCTOS.find(producto => producto.id === id);
-    CARRITO.push(productoAAgregar);
-    
-    // Total del carrito
-    let total = 0;
-    CARRITO.forEach(producto => {
-        total += producto.price
-    });
-    document.getElementById("total").innerHTML = "$"+total;
 
-    document.getElementById("cantidad").innerHTML = `Cantidad: ${CARRITO.length}`
-}
+// Onsubmit
+// function validarDatos(event){
+//     event.preventDefault();
+//     // console.log(event)
+// }
+
+// document.getElementsByTagName('form')[0].onsubmit = (event) => {
+//     event.preventDefault();
+//     console.log(event);
+// }
+
+document.getElementsByTagName("form")[0].addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    // if(event.target.children[0].getAttribute("name") == email){
+        
+    // }
+})
+
