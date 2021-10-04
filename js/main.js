a5ec79e7c25b1cfacbb8d36006da98b8483dbd93
Cristian Hourcade <hourcadecristian@gmail.com>
2021-10-04 17:00:33 -0300

    Localstorage

--- js/main.js
+++ js/main.js
@@ -1,81 +1,69 @@
-/**
- * Productos done!
- * Carrito done!
- * 
- * Funcionalidades
- * 
- * agregarAlCarrito Done!
- * SacarDelCarrito
- * SumarTotal Done!
- * Contarelementos Done!
- * 
- * mostrarLascards Done!
- */
-
-const CARRITO = [];
-
-const PRODUCTOS = [
-    { id: 1, name: "toto1", price: 900 },
-    { id: 2, name: "toto2", price: 978 },
-    { id: 3, name: "toto3", price: 345 },
-    { id: 4, name: "toto4", price: 89123 },
-    { id: 5, name: "toto5", price: 111 }
+// Creacion de ordenes de compra y venta
+const ordenes = [
+    {idMovimiento: 18396741378561, type:'short', ammount:2000 },
+    {idMovimiento: 18396741378123, type:'short', ammount:210 },
+    {idMovimiento: 23178613786912, type:'long', ammount:7900 },
+    {idMovimiento: 25151378478101, type:'short', ammount:400 },
 ]
 
-mostrarCards();
+// 9
 
-function mostrarCards() {
-    let acumular = ``;
-    PRODUCTOS.forEach(producto => {
-        acumular += `<div>
-             <h1>${producto.name}</h1>
-             <h2>$${producto.price}</h2>
-             <button data-idproducto="${producto.id}" class="btn-agregar-carrito">Agregar al carrito</button>
-         </div>`
-    });
+// Renderizar HTML
+let acumulador = ``;
 
-    document.getElementById("productos").innerHTML = acumular;
-}
+// ordenes.forEach((orden) => {
 
-const arrayDeBotones = Array.from(document.getElementsByClassName('btn-agregar-carrito')) // Conversion Array
+// });
 
-arrayDeBotones.forEach(boton => {
-    boton.onclick = (event) => {
-        const responsableID = event.target.getAttribute("data-idproducto");
-        agregarAlCarrito(responsableID)
+// ordenes.forEach(function(orden){
+//    acumulador += `
+//     <tr>
+//         <th scope="row">1</th>
+//         <td>${orden.idMovimiento}</td>
+//         <td>${orden.type}</td>
+//         <td>${orden.ammount}</td>
+//     </tr>
+//     `
+// }); 
+
+
+// document.getElementById('movimientos').innerHTML = acumulador;
+
+
+const notas = [10, 4, 7, 8, 1, 5];
+
+const notasAMostrar = notas.map(function(nota){
+    if(nota > 6){
+        return "Aprobado";
+    }else{
+        return "Desaprobado";
     }
 })
 
+console.log(notasAMostrar);
 
-function agregarAlCarrito(id) {
-    const productoAAgregar = PRODUCTOS.find(producto =>{
-        return producto.id == id;
-    }); // 1
-    CARRITO.push(productoAAgregar);
+// STORAGE: 
 
-    // Total del carrito
-    let total = 0;
-    CARRITO.forEach(producto => {
-        total += producto.price
-    });
-    document.getElementById("total").innerHTML = "$" + total;
+//Acciones
+/** ABM
+ * 
+ * Tipos de datos se pueden guardar: TEXTOS -> JSON
+ * Guardar Datos -> setItem o la asignacion a la propiedad
+ * Borrar Datos -> removeItem - clear
+ * Tomar Datos -> getItem
+ * 
+ * 'Cristian' No se necesita parsear ni convertir
+ * 
+ *                            // guardar       //traer
+ * [{id:1,nombre:'zapa}] -> JSON.Stringify -> JSON.Parse
+ * {id:1, nombre:'toto'} -> JSON.Stringify -> JSON.Parse
+ * new Producto(2, "toto") -> JSON.stringify -> JSON.parse
+ */
 
-    document.getElementById("cantidad").innerHTML = `Cantidad: ${CARRITO.length}`
-}
+// Guardado de un objeto en el LS
+const producto = new Producto(2, 'niky');
+localStorage.setItem("producto", JSON.stringify(producto));
 
-
-function eliminarDelCarrito(id) {
-    const index = CARRITO.findIndex(producto => producto.id === id);
-    if (index !== -1) {
-        CARRITO.splice(index, 1);
-    }
-
-    // Total del carrito
-    let total = 0;
-    CARRITO.forEach(producto => {
-        total += producto.price
-    });
-    document.getElementById("total").innerHTML = "$" + total;
-
-    document.getElementById("cantidad").innerHTML = `Cantidad: ${CARRITO.length}`
-}
+// La traida de un objeto del LS
+const resultado = localStorage.getItem("producto");
+console.log(JSON.parse(resultado));
